661ff2ac3250995db6156877abdef34c5eab292e
Tero Kukola <tkukola@devicebook.com>
2017-08-02 15:05:54 -0700

    Add WPF sample project equivalent to UwpMemoryLeaks

---
+++ WpfExample/WpfExample/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void showPage_Click(object sender, RoutedEventArgs e)
+        {
+            container.Content = new ContentControl();
+        }
+
+        private void hidePage_Click(object sender, RoutedEventArgs e)
+        {
+            container.Content = null;
+        }
+
+        private void gc_Click(object sender, RoutedEventArgs e)
+        {
+            GC.Collect(2, GCCollectionMode.Forced, true);
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
