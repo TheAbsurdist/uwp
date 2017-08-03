4ae233ab1603c4de4e23b52cdaaecb290bf545d3
Tero Kukola <tkukola@devicebook.com>
2017-08-03 12:38:13 -0700

    Add automated version of UwpMemoryLeaks.

---
+++ UwpMemoryLeaksAuto/UwpMemoryLeaksAuto/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UwpMemoryLeaksAuto
+{
+    public sealed partial class MainPage : Page
+    {
+        private bool running;
+        private CancellationTokenSource tokenSource;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            StartSequenceAsync();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (tokenSource != null)
+            {
+                tokenSource.Cancel();
+                tokenSource = null;
+            }
+        }
+
+        private void gc_Click(object sender, RoutedEventArgs e)
+        {
+            GC.Collect(2, GCCollectionMode.Forced, true);
+            GC.WaitForPendingFinalizers();
+        }
+
+        private async void StartSequenceAsync()
+        {
+            if (running)
+            {
+                return;
+            }
+
+            running = true;
+
+            const int PagesPerLoop = 40;
+            var start = DateTime.Now;
+            int loopCount = 0;
+            int pageCount = 0;
+
+            tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    loopCount++;
+
+                    for (int i=0; i < PagesPerLoop; i++)
+                    {
+                        container.Content = new ContentPage();
+                        pageCount++;
+                        await Task.Delay(150, token);
+                    }
+
+                    container.Content = null;
+
+                    await Task.Delay(5000, token);
+
+                    GC.Collect(2, GCCollectionMode.Forced, true);
+                    GC.WaitForPendingFinalizers();
+
+                    await Task.Delay(2000, token);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // Just ignore, this is expected.
+            }
+            finally
+            {
+                var end = DateTime.Now;
+
+                Debug.WriteLine($"End of test, loops {loopCount}");
+                Debug.WriteLine($"Total time {(end - start).Seconds} seconds");
+                Debug.WriteLine($"Number of pages created: {pageCount}");
+
+                container.Content = null;
+                tokenSource = null;
+                running = false;
+            }
+        }
+    }
+}
---
+++ UwpMemoryLeaksAuto/UwpMemoryLeaksAuto/ViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.ObjectModel;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UwpMemoryLeaksAuto
+{
+    public abstract class BaseObservable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class FooViewModel: BaseObservable
+    {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int magic;
+        public int Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public class ContentViewModel: BaseObservable
+    {
+        private ObservableCollection<FooViewModel> items = new ObservableCollection<FooViewModel>();
+        public ObservableCollection<FooViewModel> Items
+        {
+            get { return items; }
+            set
+            {
+                items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int counter;
+
+        public ContentViewModel(int nitems)
+        {
+            for (int i=0; i < nitems; i++)
+            {
+                Items.Add(CreateFoo());
+            }
+        }
+
+        private FooViewModel CreateFoo()
+        {
+            counter++;
+            var item = new FooViewModel()
+            {
+                Title = $"Foo{counter}",
+                Magic = counter,
+            };
+
+            return item;
+        }
+    }
+}
