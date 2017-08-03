403337d22ddfcfcc12ca5292cb5b2d651a378c53
Tero Kukola <tkukola@devicebook.com>
2017-08-03 13:06:25 -0700

    Add automated version of WpfExample

---
+++ WpfExampleAuto/WpfExampleAuto/ContentControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExampleAuto
+{
+    public partial class ContentControl : UserControl
+    {
+        private ContentViewModel viewModel;
+
+        public ContentControl()
+        {
+            InitializeComponent();
+            Loaded += ContentPage_Loaded;
+            Unloaded += ContentPage_Unloaded;
+        }
+
+        private void ContentPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            list.ItemsSource = null;
+            viewModel = null;
+        }
+
+        private void ContentPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel = new ContentViewModel(10000);
+            DataContext = viewModel;
+        }
+    }
+}
---
+++ WpfExampleAuto/WpfExampleAuto/FooItem.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExampleAuto
+{
+    public partial class FooItem : UserControl
+    {
+        public FooItem()
+        {
+            InitializeComponent();
+
+            Unloaded += FooItem_Unloaded;
+        }
+
+        private void FooItem_Unloaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = null;
+        }
+    }
+}
